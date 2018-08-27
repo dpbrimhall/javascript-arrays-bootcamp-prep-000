502c275566f76f6bce8f961c57a818aae526b070
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 16:18:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function removeElementFromBeginningOfArray(array) {
 
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, eight.length - 1);
+  array.slice(0, array.length - 1);
   return array
 }
 
