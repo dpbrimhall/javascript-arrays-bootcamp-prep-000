06cab6e55abac8e0dd796faabc7719b254fcf608
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 16:09:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -50,7 +50,6 @@ function removeElementFromEndOfArray(array) {
 
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  [1, 2, 3].pop()
-  array.pop();
-  return newarray
+  [1, 2, 3].pop();
+  return [1, 2, 3]
 }
